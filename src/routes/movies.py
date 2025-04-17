3323a4ac04ad35ae781d4818528e2e1ad06aaa94
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2025-04-17 15:51:43 +0300

    Solution

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -1,10 +1,52 @@
+import math
+
 from fastapi import APIRouter, Depends, HTTPException, Query
-from sqlalchemy import select
+from sqlalchemy import select, func
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import get_db, MovieModel
-
+from schemas.movies import MovieListResponseSchema, MovieDetailResponseSchema
 
 router = APIRouter()
 
-# Write your code here
+
+@router.get("/movies/{film_id}/", response_model=MovieDetailResponseSchema)
+async def get_film(film_id: int, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(MovieModel).where(MovieModel.id == film_id))
+    film = result.scalar_one_or_none()
+    if not film:
+        raise HTTPException(status_code=404, detail="Movie with the given ID was not found.")
+    return film
+
+
+@router.get("/movies/", response_model=MovieListResponseSchema)
+async def get_movies(
+        db: AsyncSession = Depends(get_db),
+        page: int = Query(1, ge=1),
+        per_page: int = Query(10, ge=1, le=100),
+):
+    total_items = await db.scalar(select(func.count()).select_from(MovieModel))
+    total_pages = math.ceil(total_items / per_page)
+
+    skip = (page - 1) * per_page
+
+    result = await db.execute(
+        select(MovieModel).order_by(MovieModel.id).offset(skip).limit(per_page)
+    )
+    movies = result.scalars().all()
+    if not movies:
+        raise HTTPException(status_code=404, detail="No movies found.")
+
+    base_url = "/theater/movies/"
+    prev_page = f"{base_url}?page={page - 1}&per_page={per_page}" if page > 1 else None
+    next_page = f"{base_url}?page={page + 1}&per_page={per_page}" if page < total_pages else None
+
+    movies_data = [MovieDetailResponseSchema.model_validate(movie).dict() for movie in movies]
+
+    return {
+        "movies": movies_data,
+        "prev_page": prev_page,
+        "next_page": next_page,
+        "total_pages": total_pages,
+        "total_items": total_items
+    }
